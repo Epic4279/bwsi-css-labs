3b5f5b1124a8e4203b915675b972ed39bf28b74c
Ameen Patel <ameen.patel.210@gmail.com>
2026-03-15 18:17:11 -0700

    Added introduction to lab_1a.py

--- labs/lab_1/lab_1a.py
+++ labs/lab_1/lab_1a.py
@@ -11,6 +11,6 @@ def main():
     name = "Ameen Patel" # TODO: Insert your name between the double quotes
 
     print(f"{name}, Welcome to the CSS course!")
-
+    print('Hello everyone, my name is Ameen Patel. I usually go by Ameen, and I am from California, currently in 11th grade. Something nerdy about me is that whenever someone brings up any numbers that I can create a mental math problem out of, I\'ll try to solve it on the spot. My favorite games include basketball, which I still need improvement on in my form, and video games like Clash Royale since it involves a development of skill and thought. My favorite "color that is not really a color" is dark denim because I really like its style.')
 if __name__ == "__main__":
     main()
